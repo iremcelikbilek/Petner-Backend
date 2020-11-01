3acf8e459e596e92307c2da42210807fcaf947ff
Yusuf Özgül <yozgul@icloud.com>
2020-11-01 18:24:36 +0300

    resolve conflics

--- Main/main.go
+++ Main/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"net/http"
 
-<<<<<<< Updated upstream
-=======
 	AdvertisementAdd "../Modules/Advertisement/Add"
 	AdvertisementGet "../Modules/Advertisement/Get"
->>>>>>> Stashed changes
 	Login "../Modules/Auth/Login"
 	NewPassword "../Modules/Auth/NewPassword"
 	Reset "../Modules/Auth/ResetPassword"
@@ -27,12 +24,9 @@ func createServer() {
 	go http.HandleFunc("/login", Login.HandleLogin)
 	go http.HandleFunc("/resetPassword", Reset.ResetPasswordHandler)
 	go http.HandleFunc("/newPassword", NewPassword.NewPassword)
-<<<<<<< Updated upstream
 
-=======
 	go http.HandleFunc("/advertisement/add", AdvertisementAdd.AdvertisementAddHandler)
 	go http.HandleFunc("/advertisement/get", AdvertisementGet.AdvertisementGetHandler)
->>>>>>> Stashed changes
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		print(err)
